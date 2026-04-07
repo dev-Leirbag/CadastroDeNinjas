cc8e544be9e8dc78878b51c004d69265ee500871
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-07 20:54:03 -0300

    feat: Atualiza o controller com um mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,17 +1,44 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //? Anotação para indicar que esta classe é um controlador REST
 @RequestMapping //? Anotação para mapear as requisições HTTP para um caminho específico (neste caso, a raiz "/")
 public class NinjaController {
 
-    //TODO: Criar um metodo GET para retornar uma mensagem de boas-vindas
-
     @GetMapping("/boasvindas") //? Anotação para mapear as requisições GET para o caminho "/boasvindas"
     public String boasVindas() {
         return "Boas vindas, esse é o meu primeiro projeto em Java 10x, estou muito feliz por isso!";
     }
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar") //? Anotação para mapear as requisições POST para o caminho "/criar")
+    public String criarNinja() {
+        return "Ninja Criado";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinja() {
+        return "Mostrar ninja";
+    }
+
+    // Mostrar ninjas por id (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjaPorId() {
+        return "Mostrar ninja por ID";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Aletar ninja por ID";
+    }
+
+    // Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por ID";
+    }
+
 }
