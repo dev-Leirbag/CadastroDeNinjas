d8d5b0c5b91b58ba28734b85a55bab8b3bbae5fa
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-07 21:06:56 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,39 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes") //? Anotação para mapear as requisições HTTP para um caminho específico (neste caso, "/missoes")
 public class MissoesController {
+
+    // Adicionar missão (CREATE)
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Criando missão";
+    }
+
+    // Mostrar todas as missoes (READ)
+    @GetMapping("/todos")
+    public String mostrarTodasAsMissoes(){
+        return "Mostrar todas as missões";
+    }
+
+    // Mostrar missoes por id (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodasAsMissoesPorId(){
+        return "Mostrar todas as missões por ID";
+    }
+
+    // Alterar dados das missoes (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarID(){
+        return "Alterar missão por ID";
+    }
+
+    // Deletar Missao (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarID(){
+        return "Deletar missão por ID";
+    }
+
 }
