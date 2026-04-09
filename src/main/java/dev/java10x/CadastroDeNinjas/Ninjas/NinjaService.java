bbd6c06151e73fbd5ab4be3849f04c9a112ee3dd
dev-Leirbag <gabrielaf2186@gmail.com>
2026-04-08 21:40:37 -0300

    feat: Adiciona a rota do delecao do ninja

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -29,4 +29,9 @@ public class NinjaService {
         return ninjaRepository.save(ninja);
     }
 
+    // Deletar o ninja - tem que ser um metodo VOID
+    public void deletarNinjaPorId (Long id) {
+        ninjaRepository.deleteById(id);
+    }
+
 }
